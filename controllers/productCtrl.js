19039fc78b905336790f6e08d32e3dd258b1bdce
ghassen <ghassenbriki123@gmail.com>
2021-05-25 19:31:44 +0100

    final commit

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,7 +1,12 @@
 const { array } = require('../middlewares/multerConfig')
 const product=require('../models/product')
+const user=require('./userCtrl')
+const userModel=require('../models/user')
+const auth=require('../middlewares/authenticate')
+const config = require("../config");
+const jwt=require('jsonwebtoken')
 
-let errors={name:'',description:'',category:'',price:''}
+let errors={name:'',description:'',tag:'',price:'',category:''}
 
 
 const handleError=(err)=>
@@ -18,6 +23,7 @@ const handleError=(err)=>
 
 exports.addProd=(req,res,next)=>
 {
+        console.log(req.user)
    
     let img=[]
 
@@ -43,11 +49,56 @@ exports.addProd=(req,res,next)=>
 
 exports.getProd=async(req,res)=>
 {
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer"))
+    {
+
+     //logged user
+    token=req.headers.authorization.split(" ")[1]
+   
+  try
+   {
+    const tokenData = jwt.verify(token, config.secretKey);
+    let currentUSer = await userModel.findById(tokenData.userId).populate('wishlist');
+  
+
+  if(currentUSer && getCategory(currentUSer))
+  
+  {
+
+ 
+ let req1= await product.find({tag:getCategory(currentUSer)}). sort({ rating: -1 }).lean()
+ let req2= await product.find( { tag: { $ne: getCategory(currentUSer)} } ).sort({ rating: -1 }).lean()
+
+ if( req2 && req1)
+ {
+ let querry=[...req2,...req1]
+res.status(200).json(querry)
+ }
+    
+}
+  
+}
+  
+
+  catch(err)
+  {
+      console.log(err)
+  }
     
 
-    try {
+  }
+
+else //visitor or user's wishlist is empty
+  
+{
+
+  
+
+  
 
            await product.find().populate('owner','username avatar totalPnts')
+           .populate('comments.author','username avatar totalPnts')
            .then((docs)=>{
                res.status(201).json(docs)
              
@@ -56,20 +107,12 @@ exports.getProd=async(req,res)=>
           
                
           
-       }
-                    
-          
-            
-    
-  catch(err)
-    {
-        res.status(403).send(err)
-    }
 
 }
 
+}
 
-
+  
 exports.like=async(req,res)=>
 {
    
@@ -156,3 +199,237 @@ catch(err)
 }
 
 
+exports.commentProd=(req,res)=>
+{
+    product.findById(req.params.prodID)
+    .then((prod)=>{
+        if(prod)
+        {   
+            if(!req.body.comment)
+            throw (new Error('comment field is empty'))
+            req.body.author = req.user._id;
+            prod.comments.push(req.body);
+            prod.save()
+            .then(()=>
+                
+                        res.status(200).json({message:'comment added succefully !'})    
+                    
+                        
+                
+            ).catch(err=>res.status(404).json(err.message));
+           
+        }
+        else{
+            err = new Error('product '+ req.params.prodID + ' not found.');
+            throw(err)
+        }
+    })
+    .catch((err)=>res.status(404).json(err.message));
+
+}
+
+
+exports.updateComment=async (req,res)=>
+{
+    try
+    {
+
+         let prod=await product.findById(req.params.prodID)
+        
+        if( prod && prod.comments.id(req.params.commentID))
+        {
+            let id1=req.user._id.toString()
+            let id2=prod.comments.id(req.params.commentID).author.toString()
+         
+            if( id1==id2 && req.body.comment )
+             {
+               
+                prod.comments.id(req.params.commentID).comment=req.body.comment
+                await prod.save()
+                
+                 return res.status(200).json({'message':'comment updated'})
+                
+             }
+             
+             
+
+             throw(new Error('you are not allowed'))
+        }
+        
+
+         throw (new Error('product or comment not found')) 
+
+        
+
+
+
+    }
+
+
+    catch(err)
+
+    {
+        res.status(403).json(err.message)
+    }
+}
+
+exports.deletComment=async (req,res)=>
+{
+    try
+    {
+
+         let prod=await product.findById(req.params.prodID)
+        
+        if( prod && prod.comments.id(req.params.commentID))
+        {
+            let id1=req.user._id.toString()
+            let id2=prod.comments.id(req.params.commentID).author.toString()
+         
+            if( id1==id2 && req.body.comment )
+             {
+               
+                prod.comments.id(req.params.commentID).remove()
+                await prod.save()
+                
+                 return res.status(200).json({message:'comment deleted'})
+                
+             }
+             
+             
+
+             throw(new Error('you are not allowed'))
+        }
+        
+
+         throw (new Error('product or comment not found')) 
+
+        
+
+
+
+    }
+
+
+    catch(err)
+
+    {
+        res.status(403).json(err.message)
+    }
+}
+
+
+exports.commentLike=async(req,res)=>
+{
+
+    try
+    {
+       let prod=await product.findById(req.params.prodID)
+   
+          if (prod && prod.comments.id(req.params.commentID))
+          {
+           if(prod.comments.id(req.params.commentID).like.indexOf(req.user._id)>=0) //if liked remove like
+              {
+                let index=prod.comments.id(req.params.commentID).like.indexOf(req.user._id)
+                prod.comments.id(req.params.commentID).like.splice(index,1)
+              }
+          
+           if(prod.comments.id(req.params.commentID).dislike.indexOf(req.user._id)>=0) //remove dislike and like
+           {
+               let index=prod.comments.id(req.params.commentID).dislike.indexOf(req.user._id)
+               prod.prod.comments.id(req.params.commentID).dislike.splice(index,1)
+               prod.comments.id(req.params.commentID).like.push(req.user._id)
+           }
+   
+           
+   
+            await prod.save()
+   
+            return res.status(200).json({message:'succes'})
+        
+     
+           
+   
+       }
+   
+       throw new Error('product not found')
+   
+      
+   
+   }
+       catch(err)
+       {
+           res.status(403).json(err.message)
+       }
+   }
+   
+exports.commentDislike=async(req,res)=>
+{
+
+    try
+    {
+       let prod=await product.findById(req.params.prodID)
+   
+          if (prod && prod.comments.id(req.params.commentID))
+          {
+           if(prod.comments.id(req.params.commentID).dislike.indexOf(req.user._id)>=0) //if disliked remove dislike
+              {
+                let index=prod.comments.id(req.params.commentID).dislike.indexOf(req.user._id)
+                prod.comments.id(req.params.commentID).dislike.splice(index,1)
+              }
+          
+           if(prod.comments.id(req.params.commentID).like.indexOf(req.user._id)>=0) //remove like and dislike
+           {
+               let index=prod.comments.id(req.params.commentID).like.indexOf(req.user._id)
+               prod.prod.comments.id(req.params.commentID).like.splice(index,1)
+               prod.comments.id(req.params.commentID).dislike.push(req.user._id)
+           }
+   
+           
+   
+            await prod.save()
+   
+            return res.status(200).json({message:'succes'})
+        
+     
+           
+   
+       }
+   
+       throw new Error('product not found')
+   
+      
+   
+   }
+       catch(err)
+       {
+           res.status(403).json(err.message)
+       }
+   }
+   
+
+
+
+
+
+
+
+
+const getCategory=(user)=>
+{
+  let occLand=0
+  let occSea=0
+  if(user.wishlist.length>0)
+  {
+    user.wishlist.forEach(el=>{
+      if(el.tag=='land')
+      occLand++
+      else
+      occSea++
+    })
+
+  if(occLand>occSea)
+  return 'land'
+  else return 'sea'
+  }
+  return null
+}
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,134 +1,125 @@
-const user=require('../models/user')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
-const config=require('../config')
+const user = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-let errors={email:'',password:'',username:''}
-
-
-const handleError=(err)=>
-{
-   if(err.message.includes("User validation failed"))
-   //console.log(err.errors)
-    Object.values(err.errors).forEach((error)=>
-   {
-    errors[error.properties.path]=error.properties.message
-   })
-
-   return errors
-}
+const Users = require("../models/user");
+const config = require("../config");
 
 
+let errors = { email: "", password: "", username: "" };
 
+const handleError = (err) => {
+  if (err.message.includes("User validation failed"))
+    //console.log(err.errors)
+    Object.values(err.errors).forEach((error) => {
+      errors[error.properties.path] = error.properties.message;
+    });
 
+  return errors;
+};
 
+exports.signUp = (req, res, next) => {
+  user
+    .findOne({ email: req.body.email })
+    .exec()
 
-exports.signUp=(req,res,next)=>
+    .then((doc) => {
+      if (doc) return res.status(401).json({ msg: "email already exist" });
 
-{
- 
+      email = req.body.email;
+      username = req.body.username;
+      password = req.body.password;
+      let avatar = "";
+      if (req.file) avatar = req.file.path;
 
-  user.findOne({email: req.body.email}).exec()
-  
-  .then(doc=>{
-    if(doc) return res.status(401).json({msg: "email already exist"});
+      const usr = new user({
+        email,
+        username,
+        password,
+        avatar,
+      });
 
-   email=req.body.email
-    username=req.body.username
-   password=req.body.password
-   let avatar=''
-  if(req.file)
-   avatar=req.file.path
-  
+      usr
+        .save()
+        .then((rslt) => {
+          // generating jwt and store it in cookie
+          const token = jwt.sign({ id: rslt._id }, config.secretKey, {
+            expiresIn: "24h",
+          }); // in s
 
-        
-        const usr = new user({
-          email,
-          username,
-          password,
-          avatar
+          /*res.cookie('jwt',token,{httpOnly:true,maxAge: 360000})*/
+          
+          res.status(200).json({
+            userId: rslt._id,
+            token: token,
+            username: usr.username,
+            email: usr.email,
+            avatar: usr.avatar,
+            wishlist: usr.wishlist,
+            totalPnts: usr.totalPnts,
+          });
         })
 
-        usr.save()
-        .then(rslt=>{
-             // generating jwt and store it in cookie
-      const token=jwt.sign({id:rslt._id,},config.secretKey,{ expiresIn: '24h' }) // in s
-      
-     /*res.cookie('jwt',token,{httpOnly:true,maxAge: 360000})*/
-       res.status(200).json({userId:rslt._id,token:token})
+        .catch((err) => res.status(400).json({ errors: handleError(err) }));
+    })
+    .catch((err) => {
+      res.status(400).json({ err: err });
+    });
+};
 
-              })
+exports.signIn = (req, res, next) => {
+  user
+    .findOne({ email: req.body.email }).populate('wishlist')
+    .exec()
+    .then((user) => {
+      if (user == null)
+        return res.status(404).json({ err: "user does not exisits !" });
 
-        .catch(err=> res.status(400).json({errors:handleError(err)}))
-         
-      
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid)
+            //valid boolean
+            return res.status(401).json({ error: "invalid password" });
 
-  }).catch(err=>{ res.status(400).json({err: err})})
+          // generating jwt and store it in cookie
+          const token = jwt.sign({ userId: user._id }, config.secretKey, {
+            expiresIn: "24h",
+          }); // in s
 
+          //res.cookie('jwt',token,{httpOnly:true,maxAge: 360000}) // in ms
+        
+        
+          res
+            .status(200)
+            .json({
+              userId: user._id,
+              token,
+              username: user.username,
+              email: user.email,
+              avatar: user.avatar,
+              wishlist: user.wishlist,
+              totalPnts: user.totalPnts,
+            });
+        })
+        .catch((err) => {
+          console.log(err), res.status(401).json({ err: err });
+        });
+    })
+    .catch((err) => res.status(401).json({ err: err }));
+};
 
-
-}
-              
-
-exports.signIn=(req,res,next)=>
-
-
-
-{
-
-  user.findOne({email:req.body.email}).exec().then((user)=>{
-    if(user==null)
-    return res.status(404).json({err:'user does not exisits !'})
-
-    bcrypt.compare(req.body.password,user.password).then((valid)=>
-    {
-      if (!valid) //valid boolean
-      return res.status(401).json({error:'invalid password'})
-      
-      // generating jwt and store it in cookie
-      const token=jwt.sign({userId:user._id,},config.secretKey,{ expiresIn: '24h' }) // in s
-      
-      //res.cookie('jwt',token,{httpOnly:true,maxAge: 360000}) // in ms
-     
-      res.status(200).json({userId:user._id,token:token})
-
-
-      
-    }).catch(err=>{console.log(err),res.status(401).json({err:err})})
-
-
-
-  }).catch(err=>res.status(401).json({err:err}))
-  
-
-  
-
-
-}    
-
-    
-exports.ranking=async (req,res)=>{
-try
-{
-  let ranking =await user.find()
-      if (ranking)
-     ranking.sort((a,b)=>
-      {
-    return b.totalPnts - a.totalPnts
-
-     })
-     return res.status(200).json(ranking)
-   
-}
-
-  catch(err)
-  {
-    res.status(403).json(err.message)
+exports.ranking = async (req, res) => {
+  try {
+    let ranking = await user.find();
+    if (ranking)
+      ranking.sort((a, b) => {
+        return b.totalPnts - a.totalPnts;
+      });
+    return res.status(200).json(ranking);
+  } catch (err) {
+    res.status(403).json(err.message);
   }
-
-
-
 }
-                      
-   
-  
+
+
